d56e56d44f496a4a6f7830d85d390ebba1380120
BessonovMark <52867045+BessonovMark@users.noreply.github.com>
2021-10-16 16:16:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// global 
+var firstNumber = 0;
+var curNumber = 0;
+var res = 0;
+var Action = String();
+var inputElement = document.getElementById("input");
+function currenter(input) {
+    if (curNumber < 1000000000000000 && curNumber > 0) {
+        curNumber *= 10;
+    }
+    curNumber += input;
+    //inputElement.innerHTML =  curNumber.toString();
+    inputElement.innerHTML = curNumber.toString();
+}
+;
+function operator(act) {
+    act();
+    inputElement.innerHTML = firstNumber.toString() + ' ' + Action;
+}
+function addition() {
+    firstNumber = curNumber;
+    curNumber = 0;
+    Action = "+";
+}
+;
+function divide() {
+    firstNumber = curNumber;
+    curNumber = 0;
+    Action = "/";
+}
+;
+function subtract() {
+    firstNumber = curNumber;
+    curNumber = 0;
+    Action = "-";
+}
+;
+function multiply() {
+    firstNumber = curNumber;
+    curNumber = 0;
+    Action = "*";
+}
+;
+function calculation() {
+    if (Action == "+") {
+        res = curNumber + firstNumber;
+    }
+    else if (Action == "*") {
+        res = curNumber * firstNumber;
+    }
+    else if (Action == "-") {
+        res = firstNumber - curNumber;
+    }
+    else if (Action == "/") {
+        res = firstNumber / curNumber;
+    }
+    inputElement.innerHTML = res.toString();
+    Action = String();
+    curNumber = res;
+    firstNumber = 0;
+}
+;
+function clearCalc() {
+    curNumber = 0;
+    firstNumber = 0;
+    inputElement.innerHTML = curNumber.toString();
+}
+;
